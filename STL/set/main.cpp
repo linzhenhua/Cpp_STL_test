5b80cba789dcc5ad05c412366e6828327b1a2e5c
linzhenhua <linzhenhualzh@163.com>
2017-02-13 17:35:17 +0800

    modify STL/set/main.cpp

--- STL/set/main.cpp
+++ STL/set/main.cpp
@@ -5,7 +5,130 @@
 
 using namespace std;
 
+bool compFun(const int &x, const int &y)
+{
+    return (x > y);
+}
+
+//仿函数
+class compClass
+{
+public:
+    bool operator() (const int &lhs, const int &rhs) const
+    {
+        return (lhs > rhs);
+    }
+};
+
+void mySet()
+{
+    set<int> mySet1();
+    //空的set不能打印输出
+    //set<int>::iterator iter = mySet1.begin();
+    //cout << *iter << endl;
+
+    int arr[10] = {0,1,2,3,4,5,6,7,8,9};
+    set<int> mySet2(arr, arr+sizeof(arr)/sizeof(int));
+    copy(mySet2.begin(), mySet2.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    set<int, compClass> mySet3(arr, arr+sizeof(arr)/sizeof(int));
+
+    //mySet3 = mySet2;
+    copy(mySet3.begin(), mySet3.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    set<int, compClass> mySet4(mySet3);
+    copy(mySet4.begin(), mySet4.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    bool (*ptrFun)(const int&, const int&) = compFun;
+
+    set<int, bool(*)(const int&, const int&)> mySet5(arr, arr+sizeof(arr)/sizeof(int), ptrFun);
+    copy(mySet5.begin(), mySet5.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+}
+
+//模板类
+template<class T>
+class RuntimeCmp{
+public:
+    enum cmp_mode{_normal, _reverse};   //0, 1
+
+private:
+    cmp_mode mode;
+
+public:
+    RuntimeCmp(cmp_mode m = _normal):mode(m){}
+
+    bool operator() (const T& t1, const T& t2) const
+    {
+        return mode == _normal ? t1 < t2 : t2 < t1;
+    }
+
+    bool operator== (const RuntimeCmp& rc)
+    {
+        return mode == rc.mode;
+    }
+};
+
+typedef set<int, RuntimeCmp<int> > IntSet;
+
+void fillElem(IntSet& _set)
+{
+    _set.insert(4);
+    _set.insert(7);
+    _set.insert(5);
+    _set.insert(1);
+    _set.insert(6);
+    _set.insert(2);
+    _set.insert(5);
+}
+
+void mySet1()
+{
+    IntSet coll1;
+    fillElem(coll1);
+
+    for(IntSet::iterator iter = coll1.begin(); iter != coll1.end(); ++iter)
+    {
+        cout << *iter << " ";
+    }
+    cout << endl;
+
+    RuntimeCmp<int> reverse_order(RuntimeCmp<int>::_reverse);
+
+    IntSet coll2(reverse_order);
+    fillElem(coll2);
+    for(IntSet::iterator iter = coll2.begin(); iter != coll2.end(); ++iter)
+    {
+        cout << *iter << " ";
+    }
+    cout << endl;
+
+    coll1 = coll2;
+    coll1.insert(3);
+    for(IntSet::iterator iter = coll1.begin(); iter != coll1.end(); ++iter)
+    {
+        cout << *iter << " ";
+    }
+    cout << endl;
+
+    if(coll1.value_comp() == coll2.value_comp())
+    {
+        cout << "coll1 and coll2 have same sorting criterion" << endl;
+    }
+    else
+    {
+        cout << "coll1 and coll2 have different sorting criterion" << endl;
+    }
+}
+
 int main()
 {
+    //mySet();
+
+    mySet1();
+
     return 0;
 }
