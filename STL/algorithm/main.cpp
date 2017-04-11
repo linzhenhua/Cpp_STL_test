eb5ea4b7f7a3814dabddc898f6edd9b6fa7dd08e
linzhenhua <linzhenhualzh@163.com>
2017-04-11 17:28:01 +0800

    modified STL/algorithm/main.cpp

--- STL/algorithm/main.cpp
+++ STL/algorithm/main.cpp
@@ -1,3 +1,5 @@
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -7,45 +9,24 @@
 
 using namespace std;
 
-void myRemove()
+int Mul2(int &x)
 {
-    vector<int> myVec;
+    return x = x*2;
+}
 
-    for(int i = 0; i != 10; ++i)
-    {
-        myVec.push_back(i);
-    }
+bool IsOdd(int x)
+{
+    return x%2 != 0;
+}
 
-    for(vector<int>::const_iterator iter = myVec.begin(); iter != myVec.end(); ++iter)
-    {
-        cout << *iter << " ";
-    }
-    cout << endl;
+void Print(int x)
+{
+    cout << x << " ";
+}
 
-    vector<int>::iterator removeIter;
-    removeIter = remove(myVec.begin(), myVec.end(), 8);
-    //remove(myVec.begin(), myVec.end(), 7);
-    //remove(myVec.begin(), myVec.end(), 1);
-
-    for(vector<int>::const_iterator iter = myVec.begin(); iter != myVec.end(); ++iter)
-    {
-        cout << *iter << " ";
-    }
-    cout << endl;
-
-    vector<int>::iterator findIter;
-    findIter = find(myVec.begin(), myVec.end(), 2);
-    myVec.erase(findIter, myVec.end());
-/*
-    vector<int>::iterator tempIter = myVec.erase(findIter, findIter+2);
-
-    cout << *tempIter << endl;
-*/
-    for(vector<int>::const_iterator iter = myVec.begin(); iter != myVec.end(); ++iter)
-    {
-        cout << *iter << " ";
-    }
-    cout << endl;
+bool myfn(int i, int j)
+{
+    return abs(i) < abs(j);
 }
 
 void myAlgorithm()
@@ -111,24 +92,57 @@ void myAlgorithm()
     */
 }
 
-int Mul2(int &x)
+void myRemove()
 {
-    return x = x*2;
-}
+    int arr[] = {0,1,2,3,4,8,6,7,8,9};
+    vector<int> myVec(arr, arr+sizeof(arr)/sizeof(int));
 
-bool IsOdd(int x)
-{
-    return x%2 != 0;
-}
+    for(vector<int>::const_iterator iter = myVec.begin(); iter != myVec.end(); ++iter)
+    {
+        cout << *iter << " ";
+    }
+    cout << endl;
 
-void Print(int x)
-{
-    cout << x << " ";
-}
+    vector<int>::iterator removeIter;
+    removeIter = remove(myVec.begin(), myVec.end(), 8);
+    //remove(myVec.begin(), myVec.end(), 7);
+    //remove(myVec.begin(), myVec.end(), 1);
 
-bool myfn(int i, int j)
-{
-    return abs(i) < abs(j);
+    myVec.erase(removeIter, myVec.end());     //物理删除
+    for(vector<int>::const_iterator iter = myVec.begin(); iter != myVec.end(); ++iter)
+    {
+        cout << *iter << " ";
+    }
+    cout << endl;
+
+    vector<int>::iterator findIter;
+    findIter = find(myVec.begin(), myVec.end(), 9);
+    if(findIter != myVec.end())
+        myVec.erase(findIter, myVec.end());
+/*
+    vector<int>::iterator tempIter = myVec.erase(findIter, findIter+2);
+
+    cout << *tempIter << endl;
+*/
+    for(vector<int>::const_iterator iter = myVec.begin(); iter != myVec.end(); ++iter)
+    {
+        cout << *iter << " ";
+    }
+    cout << endl;
+
+    vector<int> vec(arr, arr+sizeof(arr)/sizeof(int));
+    vector<int>::iterator iter;
+    iter = remove_if(vec.begin(), vec.end(), IsOdd);   //如果是奇数(使得IsOdd函数成立)的话就移除
+    vec.erase(iter, vec.end());
+    copy(vec.begin(), vec.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    vec.assign(arr, arr+sizeof(arr)/sizeof(int));
+    vector<int> vec1(11);
+    iter = remove_copy_if(vec.begin(), vec.end(), vec1.begin(), IsOdd);
+    vec1.erase(iter, vec1.end());
+    copy(vec1.begin(), vec1.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
 }
 
 void ForEach()
@@ -363,12 +377,218 @@ void Copy()
     cout << endl;
 
     vector<int> vec3;
-    vec3.assign(vec2.begin(), vec2.end());
+    vec3.assign(vec1.begin(), vec1.end());
     copy(vec3.begin(), vec3.end(), ostream_iterator<int>(cout, " "));
     cout << endl;
 
     //copy(istream_iterator<string>(cin),istream_iterator<string>(),
     //     ostream_iterator<string>(cout, "\n") );
+
+    vector<int> vec4(11);
+    copy(vec3.rbegin(), vec3.rend(), vec4.begin());
+    copy(vec4.begin(), vec4.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+}
+
+void Transform()
+{
+    int arr1[] = {0,1,2,3,3,5,0,1,7,8,9};
+    vector<int> vec( arr1, arr1 + sizeof(arr1)/sizeof(int) );
+
+    copy(vec.begin(), vec.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    vector<int> vec1;
+
+    transform(vec.begin(), vec.end(), back_inserter(vec1), bind2nd(plus<int>(), 2));
+
+    copy(vec1.begin(), vec1.end(), ostream_iterator<int>(cout, " "));
+}
+
+void Replace()
+{
+    int arr1[] = {0,1,2,3,4,5,6,7,8,9,10};
+    vector<int> vec( arr1, arr1 + sizeof(arr1)/sizeof(int) );
+
+    copy(vec.begin(), vec.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    replace(vec.begin(), vec.end(), 3, 10);
+
+    copy(vec.begin(), vec.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    replace_if(vec.begin(), vec.end(), IsOdd, 22);
+
+    copy(vec.begin(), vec.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    vector<int> vec1;
+    replace_copy(vec.begin(), vec.end(), back_inserter(vec1), 2, 11);
+
+    copy(vec1.begin(), vec1.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    vector<int> vec2;
+    replace_copy_if(vec1.begin(), vec1.end(), back_inserter(vec2), IsOdd, 12);
+
+    copy(vec2.begin(), vec2.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+}
+
+void Unique()
+{
+    int arr1[] = {0,1,1,2,3,4,5,5,5,6,7,8,9,10};
+    vector<int> vec( arr1, arr1 + sizeof(arr1)/sizeof(int) );
+
+    copy(vec.begin(), vec.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    vector<int>::iterator iter = unique(vec.begin(), vec.end());
+    vec.erase(iter, vec.end());
+    copy(vec.begin(), vec.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    vector<int> vec1;
+    vec.assign(arr1, arr1 + sizeof(arr1)/sizeof(int));
+    unique_copy(vec.begin(), vec.end(), back_inserter(vec1));
+    copy(vec1.begin(), vec1.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+}
+
+void Reverse()
+{
+    int arr1[] = {0,1,2,3,4,5,6,7,8,9,10};
+    vector<int> vec( arr1, arr1 + sizeof(arr1)/sizeof(int) );
+
+    copy(vec.begin(), vec.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    reverse(vec.begin(), vec.end());
+    copy(vec.begin(), vec.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    vector<int> vec1;
+    reverse_copy(vec.begin(), vec.end(), back_inserter(vec1));
+    copy(vec1.begin(), vec1.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+}
+
+void Rotate()
+{
+    int arr1[] = {0,1,2,3,4,5,6,7,8,9,10};
+    vector<int> vec( arr1, arr1 + sizeof(arr1)/sizeof(int) );
+
+    copy(vec.begin(), vec.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    rotate(vec.begin(), vec.begin()+1, vec.end());
+
+    copy(vec.begin(), vec.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    vector<int> vec1;
+    rotate_copy(vec.begin(), vec.begin()+1, vec.end(), back_inserter(vec1));
+
+    copy(vec1.begin(), vec1.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+}
+
+void Permutation()
+{
+    int arr[] = {1,2,3};
+
+    sort(arr, arr+sizeof(arr)/sizeof(int));
+
+    while(next_permutation(arr, arr+3))
+    {
+        cout << arr[0] << ' ' << arr[1] << ' ' << arr[2] << endl;
+    }
+    cout << "after loop:" << endl;
+    cout << arr[0] << ' ' << arr[1] << ' ' << arr[2] << endl;
+    cout << endl;
+
+    int arr1[] = {3,2,1};
+    sort(arr1, arr1+sizeof(arr1)/sizeof(int));
+    reverse(arr1, arr1+sizeof(arr1)/sizeof(int));
+
+    while(prev_permutation(arr1, arr1+3))
+    {
+        cout << arr1[0] << ' ' << arr1[1] << ' ' << arr1[2] << endl;
+    }
+    cout << "after loop:" << endl;
+    cout << arr1[0] << ' ' << arr1[1] << ' ' << arr1[2] << endl;
+
+    int arr2[] = {1,2,3,4,5};
+    sort(arr2, arr2+sizeof(arr2)/sizeof(int));
+
+    int x = 1;   //要包含原来的1,2,3,4,5这个次序，所以x是1
+    while(next_permutation(arr2, arr2+5))
+    {
+        x++;
+    }
+    cout << x << endl;
+}
+
+int MyRandom(int i)
+{
+    return rand()%i;
+}
+
+void RandomShuffle()
+{
+    int arr1[] = {0,1,2,3,4,5,6,7,8,9,10};
+    vector<int> vec( arr1, arr1 + sizeof(arr1)/sizeof(int) );
+
+    copy(vec.begin(), vec.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    random_shuffle(vec.begin(), vec.end());
+
+    copy(vec.begin(), vec.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    srand((unsigned int)time(0));     //初始化随机数生成器
+
+    vector<int> vec1( arr1, arr1 + sizeof(arr1)/sizeof(int) );
+
+    random_shuffle(vec1.begin(), vec1.end(), MyRandom);
+    copy(vec1.begin(), vec1.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+}
+
+void Partition()
+{
+    int arr1[] = {0,1,2,3,4,5,6,7,8,9,10};
+    vector<int> vec( arr1, arr1 + sizeof(arr1)/sizeof(int) );
+
+    copy(vec.begin(), vec.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    partition(vec.begin(), vec.end(), IsOdd);
+
+    copy(vec.begin(), vec.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    sort(vec.begin(), vec.end());
+
+    stable_partition(vec.begin(), vec.end(), IsOdd);
+    copy(vec.begin(), vec.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+}
+
+void Sort()
+{
+    int arr1[] = {9,8,7,6,5,4,3,2,1};
+    vector<int> vec( arr1, arr1 + sizeof(arr1)/sizeof(int) );
+
+    copy(vec.begin(), vec.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    partial_sort(vec.begin(), vec.begin()+3, vec.end());
+
+    copy(vec.begin(), vec.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
 }
 
 int main()
@@ -397,9 +617,25 @@ int main()
 
     //Mismatch();
 
-    Copy();
+    //Copy();
 
+    //Transform();
 
+    //Replace();
+
+    //Unique();
+
+    //Reverse();
+
+    //Rotate();
+
+    //Permutation();
+
+    //RandomShuffle();
+
+    //Partition();
+
+    Sort();
 
     return 0;
 }
