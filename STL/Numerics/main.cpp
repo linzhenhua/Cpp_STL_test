21919dc28e1884a50b1cb5473c0a25eaf64391e3
linzhenhua <linzhenhualzh@163.com>
2017-04-21 17:49:49 +0800

    modified STL/Numerics/main.cpp

--- STL/Numerics/main.cpp
+++ STL/Numerics/main.cpp
@@ -7,9 +7,18 @@ using namespace std;
 
 void Complex()
 {
-    complex<int> first(1,1);
+    complex<int> first(1,2);
+    complex<int> second(3,4);
 
     cout << first << endl;
+    cout << first.real() << endl;
+    cout << first + second << endl;
+
+    //cin >> first;
+    //cout << first << endl;
+
+    complex<int> third( conj(second) );
+    cout << third << endl;
 }
 
 int main()
